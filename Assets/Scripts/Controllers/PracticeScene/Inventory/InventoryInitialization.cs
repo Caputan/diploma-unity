c665965df091727c6d792d5777f33e34bb3692f4
Artem <zmet1999@mail.ru>
2021-03-30 16:03:27 +0300

    Inventory init

---
+++ Assets/Scripts/Controllers/PracticeScene/Inventory/InventoryInitialization.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using Diploma.Controllers;
+using Diploma.Interfaces;
+using Diploma.UI;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Controllers.PracticeScene.Inventory
+{
+    public class InventoryInitialization: IInitialization
+    {
+        private readonly GameContextWithViews _gameContextWithViews;
+        private readonly GameContextWithUI _gameContextWithUI;
+        private readonly GameObject _mainParent;
+        private readonly GameObject _inventoryPrefab;
+        private readonly InventoryFactory _inventoryFactory;
+        private List<Button> InventoryButtons;
+
+        private PlayerController _player;
+        private GameObject[] _parts;
+        
+
+        public InventoryInitialization(GameContextWithViews gameContextWithViews,
+            GameContextWithUI gameContextWithUI, GameObject MainParent, GameObject inventoryPrefab, 
+            GameObject[] parts, PlayerController player)
+        {
+            _gameContextWithViews = gameContextWithViews;
+            _gameContextWithUI = gameContextWithUI;
+            _mainParent = MainParent;
+            _inventoryPrefab = inventoryPrefab;
+            _parts = parts;
+            _player = player;
+            _inventoryFactory = new InventoryFactory(_inventoryPrefab);
+        }
+        
+        public void Initialization()
+        {
+            var inventory = _inventoryFactory.Create(_mainParent.transform);
+            
+            inventory.transform.localPosition = Vector3.zero;
+
+            InventoryButtons = new List<Button>();
+            InventoryButtons.AddRange(inventory.GetComponentsInChildren<Button>());
+
+            new InventoryAddButtonsToDictionary(InventoryButtons, _gameContextWithViews, _parts);
+            var inventoryLogic = new InventoryLogic(_gameContextWithViews.InventoryButtons, _player);
+            inventoryLogic.Initialization();
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Inventory/InventoryLogic.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Diploma.Enums;
+using Diploma.Interfaces;
+using Interfaces;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Diploma.UI
+{
+    public class InventoryLogic: IInitialization
+    {
+        private readonly Dictionary<GameObject, Button> _buttons;
+        private readonly PlayerController _player;
+
+        public InventoryLogic(Dictionary<GameObject, Button> buttons, PlayerController player)
+        {
+            _player = player;
+            _buttons = buttons;
+        }
+
+        private void GiveToPlayer(GameObject objectToPick)
+        {
+            _player.PickUp(objectToPick);
+        }
+        
+        public void Initialization()
+        {
+            foreach (var button in _buttons)
+            {
+                button.Value.onClick.RemoveAllListeners();
+                button.Value.onClick.AddListener(() => GiveToPlayer(button.Key));
+            }
+        }
+    }
+}
