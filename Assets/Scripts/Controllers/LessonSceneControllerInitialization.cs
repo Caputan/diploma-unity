d5b7bb5a2fd3597a54789269ef133e54e6135d93
Jastot <george1005@yandex.ru>
2021-03-30 12:38:10 +0300

    SceneInitializationAdded

---
+++ Assets/Scripts/Controllers/LessonSceneControllerInitialization.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+
+
+namespace Diploma.Controllers
+{
+    public class LessonSceneControllerInitialization: MonoBehaviour
+    {
+
+        private Controllers _controllers;
+        public string[] destinationPath = new string[4];
+        public void Start()
+        {
+            
+            
+            _controllers = new Controllers();
+            
+            _controllers.Initialization();
+        }
+        
+        private void Update()
+        {
+            var deltaTime = Time.deltaTime;
+            _controllers.Execute(deltaTime);
+        }
+
+        private void LateUpdate()
+        {
+            var deltaTime = Time.deltaTime;
+            _controllers.LateExecute(deltaTime);
+        }
+
+        private void OnDestroy()
+        {
+            _controllers.CleanData();
+        }
+    }
+}
