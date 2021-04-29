d2e83dd678124e5663c2295a43b55129f5892519
Jastot <george1005@yandex.ru>
2021-04-29 15:38:55 +0300

    CryptoStart

---
+++ Assets/Scripts/Crypto/CryptoGrath.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using System.Text;
+
+namespace Crypto
+{
+    public sealed class CryptoGrath
+    {
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            var hmac = new System.Security.Cryptography.HMACSHA512();
+            passwordSalt = hmac.Key;
+            passwordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+        }
+        
+        private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt);
+            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+            return !computedHash.Where((x, i) => x != passwordHash[i]).Any();
+        }
+    }
+}
