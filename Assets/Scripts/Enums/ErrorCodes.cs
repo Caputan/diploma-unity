ad2347e7c629a5ad1bb88977e50ba4631489004a
Jastot <george1005@yandex.ru>
2021-05-20 20:32:01 +0300

    lost error codes...nm

--- Assets/Scripts/Enums/ErrorCodes.cs
+++ Assets/Scripts/Enums/ErrorCodes.cs
@@ -7,5 +7,8 @@
         SignUpError = 2,
         EmptyInputError = 3,
         WrongFormatError = 4,
+        ValidationLoginError = 5,
+        ValidationPasswordError = 6,
+        ValidationEmailError = 7,
     }
 }
